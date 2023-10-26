8e835da180b63b8375bdb7c3632a8f116120fdf5
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2023-10-26 10:58:12 +0800

    third

--- factory.php
+++ factory.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Factory Table</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-
-  <?php
+<?php
     include("index.php");
 
     interface IUserFactory {
@@ -96,8 +84,4 @@
         echo '</tbody>';
         echo '</table>';
     }
-  ?>
-
-</body>
-
-</html>
+?>
